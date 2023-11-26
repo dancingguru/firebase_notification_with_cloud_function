fa680e94d02da09f9a9f4b3005201fde21f28e71
dancingguru <lionmarksham@gmail.com>
2023-11-26 10:50:35 -0500

    remove some config files

--- functions/index.js
+++ functions/index.js
@@ -2,14 +2,14 @@ const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 
 admin.initializeApp({
-  apiKey: "AIzaSyDGE62OVLm4tBqmBOeWvXAA6L_7XxLEm4A",
-  authDomain: "point-citi.firebaseapp.com",
-  databaseURL: "https://point-citi-default-rtdb.firebaseio.com",
-  projectId: "point-citi",
-  storageBucket: "point-citi.appspot.com",
-  messagingSenderId: "593532867918",
-  appId: "1:593532867918:web:7975ecd086e0890457ba1f",
-  measurementId: "G-XRLEG2M9PV",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 });
 
 exports.sendStoreCatalog = functions.firestore
